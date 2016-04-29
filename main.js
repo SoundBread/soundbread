8fa7e4a11d1b1ed5c44de44cc286c29dbfc0f77e
Edwin Schaap <edwin@schaap.me>
2016-04-29 10:03:38 +0200

    Op basis van Electron kan er een applicatie executable worden gecompileerd

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-closed', function(){
+  app.quit();
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400
+  });
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
